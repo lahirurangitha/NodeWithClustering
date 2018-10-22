26449ab7f912f78a94095f7a98d5a2ff66258187
lahirurangitha <lahirupathiranalr@gmail.com>
2018-10-22 06:18:22 +0530

    update

--- server/server.js
+++ server/server.js
@@ -1,45 +1,44 @@
 const http = require('http');
 const dt = require('./dateTime_module');
-// const fs = require('fs');
-const hl = require('./htmlLoader_module');
+const fs = require('fs');
+// const hl = require('./htmlLoader_module');
 
 const hostname = 'localhost';
 const port = 8000;
 
+let count = 0;
+
 let handleRequest = (request, response) => {
+
+  console.log('Request received.'+count++);
   response.statusCode = 200;
   response.setHeader('Content-Type', 'text/html');
-
-
 
   //routing
   let url = request.url;
   if (url === '/about') {
-    response.write('<h1>about us page<h1>'); //write a response
+    response.write('<h1>about us page<h1>');
   } else if (url === '/contact') {
-    response.write('<h1>contact us page<h1>'); //write a response
+    response.write('<h1>contact us page<h1>');
   } else {
-      let content = hl.getContent();
-      // console.log(content);
-      response.write(content);
-    // fs.readFile('./index.html', function (error, html) {
-    //   if (error) {
+    response.write('<h1>process ' + process.pid + ' says hello! <h1>');
+    //   fs.readFile('../client/index.html', function (error, html) {
+    //     if (error) {
 
-    //     response.writeHead(404);
-    //     respone.write('file not found');
-    //   } else {
-    //     response.write(html);
-    //   }
-    //   response.end();
-    // });
+    //       response.writeHead(404);
+    //       respone.write('file not found');
+    //     } else {
+    //       response.write(html);
+    //     }
+    //     response.end();
+    //   });
+  }
+  response.end();
+  console.log('process ' + process.pid + ' says hello!');
+  while(true){
 
   }
-
 }
-
-// response.write("The date and time are currently: " + dt.myDateTime());
-// response.end();
-// }
 
 // create a server object.
 // The function passed into the http.createServer() method 
